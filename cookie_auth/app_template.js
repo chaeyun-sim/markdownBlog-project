b2bc4f051f648e64649e2c7a839fe3723f95dd42
chaeyunsim <bysimune@gmail.com>
2022-11-12 23:20:46 +0900

    updated

--- cookie_auth/app_template.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.listen(3003, () => {
-    console.log('Connected to port 3003')
-})
--- practice/underscore.js
+++
@@ -1,3 +0,0 @@
-var under = require('./underscore');
-var arr = [3, 6, 9, 1, 12];
-console.log(under.last(arr));
